29bab3692d2b4e95f117e04660cf1343be58c2ae
Sergio Suárez <triplesxulo@gmail.com>
2019-10-14 10:27:13 +0200

    agrego un update

--- update.sql
+++ update.sql
@@ -1,3 +1,7 @@
 update Usuarios
 set edad=20
-where nombre like "Pepe" and Apellido like "Perez"
+where nombre like "Pepe" and Apellido like "Perez";
+
+update Curso
+set nombre=SMR
+where nombre like "Francisco" and Apellido like "López"
