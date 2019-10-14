1457cf141c6627ac63e328b2c681a9024842df65
Rafael Bohorquez Gomez <rafaelgomezbohorquez2000@gmail.com>
2019-10-14 10:29:28 +0200

    Update aula

--- update.sql
+++ update.sql
@@ -5,3 +5,7 @@ where nombre like "Pepe" and Apellido like "Perez";
 update Curso
 set nombre=SMR
 where nombre like "Francisco" and Apellido like "López"
+
+update Aula
+set codigo=2343M
+where nombre like "Francisco"
